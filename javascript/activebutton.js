ad83b9aca805ee75a1b581363e8329dc7d07f8e2
Yamairo <yamairo10@gmail.com>
2022-12-17 11:47:25 +0100

    app homescreen verbeterd en bestanden voor href toegevoegd

--- javascript/activebutton.js
+++ javascript/activebutton.js
@@ -1,5 +1,9 @@
 document.querySelectorAll(".button-nav").forEach(
     link=>{
-        console.log(link.onclick)
+        console.log(link.href)
+        console.log(window.location.href)
+        if(link.href===window.location.hre){
+            link.setAttribute("btn", "page")
+        }
     }
 )
