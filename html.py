263ef8afb986d460162f306f6522d29cad538f93
Yamairo <yamairo10@gmail.com>
2023-01-21 17:35:14 +0100

    `Barcode scanner met popup elke seconde

--- html.py
+++ html.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # Open the JSON file and read the contents
-with open("recipes.json", "r") as f:
+with open("json/recipes.json", "r") as f:
     recipes = json.load(f)
 
 # Iterate through each recipe in the JSON file
@@ -16,25 +16,53 @@ for recipe in recipes:
 
     # Create a new HTML file with the recipe name and href
     with open(f"{href}", "w") as f:
-        f.write("<!DOCTYPE html>")
-        f.write("<html>")
-        f.write("<head>")
-        f.write(f"<title>{name}</title>")
-        f.write("</head>")
-        f.write("<body>")
-        f.write(f"<h1>{name}</h1>")
-        f.write(f"<p>{description}</p>")
-        f.write("<h2>Ingredients:</h2>")
-        f.write("<ul>")
-        for ingredient in ingredients:
-            f.write(f"<li>{ingredient}</li>")
-        f.write("</ul>")
-        f.write("<h2>Instructions:</h2>")
-        f.write("<ol>")
-        for instruction in instructions:
-            f.write(f"<li>{instruction}</li>")
-        f.write("</ol>")
-        f.write("</body>")
-        f.write("</html>")
-
-print("HTML files have been generated and saved to the html_files folder.")
+        f.write("<!DOCTYPE html>\n")
+        f.write("<html>\n")
+        f.write("\t<head>\n")
+        f.write("\t\t<link rel='stylesheet' href='css/recipe_page.css'>\n")
+        f.write("\t\t<title>" + name + "</title>\n")
+        f.write("\t</head>\n")
+        f.write("\t<body>\n")
+        f.write("\t\t<div id='linkContainer'>\n")
+        f.write("\t\t\t<a href='recipes.html'>Terug naar recepten</a>\n")
+        f.write("\t\t</div>\n")
+        f.write("\t\t<div id='container'>\n")
+        f.write("\t\t\t<img id='img' src='img/background.jpg' alt=''>\n")
+        f.write("\t\t\t<div id='div1'>\n")
+        f.write(
+            "\t\t\t\t<!-- De naam van het gerecht word met js code geinjecteerd-->\n")
+        f.write("\t\t\t</div>\n")
+        f.write("\t\t</div>\n")
+        f.write("\t\t\t<div id='div2'>\n")
+        f.write("\t\t\t\t<h2>Ingredienten:</h2>\n")
+        f.write("\t\t\t\t<!-- Ingredienten worden geinjecteerd met js code -->\n")
+        f.write("\t\t\t</div>\n")
+        f.write("\t\t\t<div id='div3'>\n")
+        f.write("\t\t\t\t<h2 id='instructies'>Instructies:</h2>\n")
+        f.write("\t\t\t\t<ul id='instructies'></ul>\n")
+        f.write("\t\t\t</div>\n")
+        f.write("\t</body>\n")
+        f.write("\t<script src='javascript/liveserver.js'>\n")
+        f.write("\t</script>\n")
+        f.write("\t\t<div id='linkContainer'>\n")
+        f.write("\t\t\t<a href='recipes.html'>Terug naar recepten</a>\n")
+        f.write("\t\t</div>\n")
+        f.write("\t\t<div id='container'>\n")
+        f.write("\t\t\t<img id='img' src='img/background.jpg' alt=''>\n")
+        f.write("\t\t\t<div id='div1'>\n")
+        f.write(
+            "\t\t\t\t<!-- De naam van het gerecht word met js code geinjecteerd-->\n")
+        f.write("\t\t\t</div>\n")
+        f.write("\t\t</div>\n")
+        f.write("\t\t<div id='div2'>\n")
+        f.write("\t\t\t<h2>Ingredienten:</h2>\n")
+        f.write("\t\t\t<!-- Ingredienten worden geinjecteerd met js code -->\n")
+        f.write("\t\t</div>\n")
+        f.write("\t\t<div id='div3'>\n")
+        f.write("\t\t\t<h2 id='instructies'>Instructies:</h2>\n")
+        f.write("\t\t\t<ul id='instructies'></ul>\n")
+        f.write("\t\t</div>\n")
+        f.write("\t</body>\n")
+        f.write("\t<script src='javascript/liveserver.js'></script>\n")
+        f.write("\t<script src='javascript/recipes.js'></script>\n")
+        f.write("</html>\n")
