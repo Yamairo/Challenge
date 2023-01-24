6cd9120810f16838737a658d61db9e1fd15a8652
Yamairo <yamairo10@gmail.com>
2023-01-24 14:36:55 +0100

    PLEAAAASE ONG JUST WORK

--- views.py
+++ views.py
@@ -9,3 +9,11 @@ def product_data(request, product_code):
 
     context = {'product_name': product_name, 'product_image': product_image}
     return render(request, 'product_data.html', context)
+
+
+def product(product_code):
+    product_data = openfoodfacts.products.get_product(product_code)["product"]
+    product_name = product_data["product_name"]
+    product_image = product_data["selected_images"]["front"]["display"]["fr"]
+
+    context = {'product_name': product_name, 'product_image': product_image}
